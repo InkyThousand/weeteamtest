0d02e93af318ea764f25fe07a944ac7a63d4456b
Pavel Losiev <plosiev@acceptic.com>
2020-03-30 01:22:05 +0300

    add entity, repository, other configs

---
+++ app/src/Controller/DossierController.php
@@ -0,0 +1,125 @@
+<?php
+namespace App\Controller;
+
+use App\Entity\Dossier;
+use App\Form\DossierType;
+use App\Repository\DossierRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use http\Env\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\FrameworkBundle\Controller\RedirectController;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\RouterInterface;
+
+class DossierController extends AbstractController
+{
+
+    /**
+     * @var DossierRepository
+     */
+    private $dossierRepository;
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $entityManager;
+
+    public function __construct(DossierRepository $dossierRepository, RouterInterface $router, FormFactoryInterface $formFactory, EntityManagerInterface $entityManager)
+    {
+        $this->dossierRepository = $dossierRepository;
+        $this->router = $router;
+        $this->formFactory = $formFactory;
+        $this->entityManager = $entityManager;
+    }
+
+    /**
+     * @Route("/", name="index", options={ "method_prefix" = false })
+     */
+    public function indexAction()
+    {
+        return $this->render(
+            'index.html.twig',
+            [
+                'users' => $this->dossierRepository->findBy(array('status' => true))
+            ]
+        );
+    }
+
+    /**
+     * @Route("/edit/{id}", name="edit_dossier", options={ "method_prefix" = false })
+     * @param $id
+     * @param Request $request
+     * @return RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function editAction($id, Request $request)
+    {
+        $dossier = $this->dossierRepository->find($id);
+        $form = $this->createForm(DossierType::class, $dossier);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $this->entityManager->persist($dossier);
+            $this->entityManager->flush();
+
+            return new RedirectResponse($this->router->generate('index'));
+        }
+
+        return $this->render(
+            'crud.html.twig',
+            ['form' => $form->createView()]
+        );
+
+    }
+
+    /**
+     * @Route("/add", name="add_dossier", options={ "method_prefix" = false })
+     * @param Request $request
+     * @return RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function addAction(Request $request)
+    {
+        $dossier = new Dossier();
+        $form = $this->createForm(DossierType::class, $dossier);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted()){
+            $this->entityManager->persist($dossier);
+            $this->entityManager->flush();
+
+            return new RedirectResponse($this->router->generate('index'));
+        }
+
+        return $this->render(
+            'crud.html.twig',
+            ['form' => $form->createView()]
+        );
+    }
+
+    /**
+     * @Route("/delete/{id}", name="delete_dossier", options={ "method_prefix" = false })
+     * @param $id
+     * @return RedirectResponse
+     */
+    public function deleteAction($id)
+    {
+        $dossier = $this->dossierRepository->find($id);
+        $dossier->setStatus(false);
+
+        $this->entityManager->persist($dossier);
+        $this->entityManager->flush();
+
+        return new RedirectResponse($this->router->generate('index'));
+    }
+}
---
+++ app/src/Entity/Dossier.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\DossierRepository")
+ */
+class Dossier
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $dossierNumber;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $dossierType;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $firstName;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $lastName;
+
+    /**
+     * @ORM\Column(type="string", length=255, nullable=true)
+     */
+    private $address;
+
+    /**
+     * @ORM\Column(type="integer", nullable=true)
+     */
+    private $creditCard;
+
+    /**
+     * @ORM\Column(type="decimal", precision=4, scale=0, nullable=true)
+     */
+    private $cvvCard;
+
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $status = true;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getDossierNumber(): ?int
+    {
+        return $this->dossierNumber;
+    }
+
+    public function setDossierNumber(int $dossierNumber): self
+    {
+        $this->dossierNumber = $dossierNumber;
+
+        return $this;
+    }
+
+    public function getDossierType(): ?string
+    {
+        return $this->dossierType;
+    }
+
+    public function setDossierType(?string $dossierType): self
+    {
+        $this->dossierType = $dossierType;
+
+        return $this;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(?string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(?string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+
+    public function getCreditCard(): ?int
+    {
+        return $this->creditCard;
+    }
+
+    public function setCreditCard(?int $creditCard): self
+    {
+        $this->creditCard = $creditCard;
+
+        return $this;
+    }
+
+    public function getCvvCard(): ?string
+    {
+        return $this->cvvCard;
+    }
+
+    public function setCvvCard(?string $cvvCard): self
+    {
+        $this->cvvCard = $cvvCard;
+
+        return $this;
+    }
+
+    public function getStatus(): ?bool
+    {
+        return $this->status;
+    }
+
+    public function setStatus(?bool $status): self
+    {
+        $this->status = $status;
+
+        return $this;
+    }
+}
